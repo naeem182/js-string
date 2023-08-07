e367372b6892ba0ea7ddcfb4142b2740bf1080ae
naeem <naeemislamjr182@gmail.com>
2023-08-07 19:41:36 +0600

    convert case

---
+++ case_change.js
@@ -0,0 +1,10 @@
+const username = 'Naeem';
+const userInput = 'naeem';
+if (username.toLowerCase() === userInput.toLowerCase()) {
+    console.log("valid");
+
+}
+else {
+    console.log('not valid');
+
+}
