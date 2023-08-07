e8b7cf348d212ea51dd18af077f02b2523226e7e
naeem <naeemislamjr182@gmail.com>
2023-08-07 22:25:42 +0600

    function max with condition

--- fun_max.js
+++ fun_max.js
@@ -1,1 +1,16 @@
+//with condition function
+function max_num(jim, dela, chinku) {
 
+    if (jim > dela && jim > chinku) {
+        console.log('Jim will get the cake');
+    }
+    else if (dela > jim && dela > chinku) {
+        console.log('Dela will eat cake in this bela');
+    }
+    else {
+        console.log('Chinku will get the cake');
+    }
+}
+
+
+console.log(max_num(34, 75, 63))
