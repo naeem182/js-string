da79da99ca86dd6ea0b13bbc01497c7f467ee844
naeem <naeemislamjr182@gmail.com>
2023-08-07 20:02:38 +0600

    found string or not

---
+++ serchi_in_string.js
@@ -0,0 +1,4 @@
+const ganner_koli = 'tumi bondhu kala pakhi ami jeno ki';
+const serch = 'pafkhi';
+const doesExist = ganner_koli.toLowerCase().includes(serch.toLowerCase());
+console.log(doesExist)
