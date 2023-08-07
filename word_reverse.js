3d52090579b030320dcc089dc08fb94d5136b249
naeem <naeemislamjr182@gmail.com>
2023-08-08 00:00:03 +0600

    reversed word by split and join

--- word_reverse.js
+++ word_reverse.js
@@ -8,22 +8,7 @@ function reverseWords(str) {
         result.push(element);
     }
     const reversed = result.join(' ');
-    console.log(reversed);
-}
-
-
-const myString = 'I am a good boy';
-console.log(reverseWords(myString));
-function reverseWords(str) {
-    const words = str.split(' ');
-    const result = [];
-    // [ 'I', 'am', 'a', 'good', 'boy' ]
-    for (let i = words.length - 1; i >= 0; i--) {
-        const element = words[i];
-        result.push(element);
-    }
-    const reversed = result.join(' ');
-    console.log(reversed);
+    return reversed;
 }
 
 
