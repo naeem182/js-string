539b66246c8f1cf3ccf6027de2932afd0d1debf1
naeem <naeemislamjr182@gmail.com>
2023-08-07 20:33:49 +0600

    .split(kivabe vangbo) mne vanga-vangir khela

---
+++ split_slice_join.js
@@ -0,0 +1,10 @@
+const ganner_koli = 'tumi bondhu kala Pakhi ami jeno ki. bosonto kale tomai bolte pari ni. kala kala sada sada';
+
+const line_break_sent = ganner_koli.split('.');//split er vitor bujay dite hbe kivabe vangbe ---->sentence
+console.log(line_break_sent)
+
+const line_break = ganner_koli.split(' ');//split er vitor bujay dite hbe kivabe vangbe ---->word
+console.log(line_break)
+
+const line_break_char = ganner_koli.split('');//split er vitor bujay dite hbe kivabe vangbe ---->char
+console.log(line_break_char)
