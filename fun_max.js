e20493223f58c3f8338015abe8fd1c048b8befe9
naeem <naeemislamjr182@gmail.com>
2023-08-07 22:48:18 +0600

    function max with array  and returnn

--- fun_max.js
+++ fun_max.js
@@ -1,4 +1,4 @@
-// //with condition function
+// //with condition function................
 // function max_num(jim, dela, chinku) {
 
 //     if (jim > dela && jim > chinku) {
@@ -15,21 +15,55 @@
 
 // console.log(max_num(34, 75, 63))
 
-// with condition function
-function max_num(jim, dela, chinku) {
+// with condition function..............
+// function max_num(jim, dela, chinku) {
 
-    if (Math.max(jim, dela, chinku) === jim) {
-        return 'Jim will get the cake';
+//     if (Math.max(jim, dela, chinku) === jim) {
+//         return 'Jim will get the cake';
+//     }
+
+//     else if (Math.max(jim, dela, chinku) === dela) {
+//         return 'dela will get the cake';
+//     }
+//     else {
+//         return 'chinku will get the cake';
+//     }
+
+// }
+
+
+// console.log(max_num(64, 75, 93))
+
+//............return function with array
+
+
+function findLargest(x) {
+
+    let largest = x[0];
+
+    for (let i = 0; i < x.length; i++) {
+        const index = i;
+
+        const element = x[i];
+
+        if (element > largest) {
+            largest = element;
+        }
+
     }
 
-    else if (Math.max(jim, dela, chinku) === dela) {
-        return 'dela will get the cake';
-    }
-    else {
-        return 'chinku will get the cake';
-    }
+    // for(let i of x){
+    //     const element =i
+
+    //     if(element > largest ){
+    //         largest = element
+    //     }
+    // }
+
+    return largest;
 
 }
 
 
-console.log(max_num(64, 75, 93))
+const arr = [56, 12, 7, 9, 20, 4, 2, 3];
+console.log(findLargest(arr))
