fd42bbcdf248b20779c982fc894d1bfbc97a9b39
naeem <naeemislamjr182@gmail.com>
2023-08-07 22:00:45 +0600

    max with condition

---
+++ max.js
@@ -0,0 +1,13 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if (jim > dela && jim > chinku) {
+    console.log('Jim will get the cake');
+}
+else if (dela > jim && dela > chinku) {
+    console.log('Dela will eat cake in this bela');
+}
+else {
+    console.log('Chinku will get the cake');
+}
