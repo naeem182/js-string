bf15bf0a8f1e42e8ea3d5bf842f4667b207fdfa4
naeem <naeemislamjr182@gmail.com>
2023-08-07 22:27:03 +0600

    function max with max functioin and returnn

--- fun_max.js
+++ fun_max.js
@@ -1,16 +1,35 @@
-//with condition function
+// //with condition function
+// function max_num(jim, dela, chinku) {
+
+//     if (jim > dela && jim > chinku) {
+//         console.log('Jim will get the cake');
+//     }
+//     else if (dela > jim && dela > chinku) {
+//         console.log('Dela will eat cake in this bela');
+//     }
+//     else {
+//         console.log('Chinku will get the cake');
+//     }
+// }
+
+
+// console.log(max_num(34, 75, 63))
+
+// with condition function
 function max_num(jim, dela, chinku) {
 
-    if (jim > dela && jim > chinku) {
-        console.log('Jim will get the cake');
+    if (Math.max(jim, dela, chinku) === jim) {
+        return 'Jim will get the cake';
     }
-    else if (dela > jim && dela > chinku) {
-        console.log('Dela will eat cake in this bela');
+
+    else if (Math.max(jim, dela, chinku) === dela) {
+        return 'dela will get the cake';
     }
     else {
-        console.log('Chinku will get the cake');
+        return 'chinku will get the cake';
     }
+
 }
 
 
-console.log(max_num(34, 75, 63))
+console.log(max_num(64, 75, 93))
