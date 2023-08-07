7d9eebd39bbec91dc228def84783a7ef85cbbe6c
naeem <naeemislamjr182@gmail.com>
2023-08-07 20:11:51 +0600

    found string or not with indexof() function

--- serchi_in_string.js
+++ serchi_in_string.js
@@ -1,4 +1,18 @@
 const ganner_koli = 'tumi bondhu kala pakhi ami jeno ki';
-const serch = 'pafkhi';
-const doesExist = ganner_koli.toLowerCase().includes(serch.toLowerCase());
-console.log(doesExist)
+// const serch = 'pafkhi';
+// const doesExist = ganner_koli.toLowerCase().includes(serch.toLowerCase());
+// console.log(doesExist)
+//...............................
+// indexOf
+
+// console.log(ganner_koli.indexOf('kala'));//takes start string kala------->k=12
+// console.log(ganner_koli.indexOf('Tumi'))//if not found then index num -1
+// 
+
+if (ganner_koli.indexOf('pakhi') !== -1) {
+    console.log('exists inside the string')
+    console.log("index start with", ganner_koli.indexOf('pakhi'))
+}
+else {
+    console.log('cannot find it');
+}
